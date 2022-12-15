dd105d04eab5c2c2a7e8135152c3ba227968988c
ilyagerman52 <ivgerman@edu.hse.ru>
2022-12-15 21:20:11 +0300

    add variants

--- Bot_Class.py
+++ Bot_Class.py
@@ -1,3 +1,4 @@
+import random
 import time
 import telebot
 from telebot import types
@@ -12,9 +13,9 @@ from create_good_name import good_name
 class Chat:
     premessage = ''
     waiting_answer = None
-    last_asked_message = None
     streak = 0
     category = None
+    ans_hidden = True
 
 
 class Bot:
@@ -49,7 +50,10 @@ class Bot:
             markup.add(but_y, but_n)
             self.bot.send_message(chat_id, HELLO_MESSAGE, reply_markup=markup)
         elif t == 'help':
-            self.bot.send_message(chat_id, HELP_MESSAGE, )
+            markup = types.InlineKeyboardMarkup()
+            but_start = types.InlineKeyboardButton(text='Начать', callback_data='change_category')
+            markup.add(but_start)
+            self.bot.send_message(chat_id, HELP_MESSAGE, reply_markup=markup)
         elif t == 'choose_category':
             message_text = 'Выбери тему.'
             markup = types.InlineKeyboardMarkup(row_width=1)
@@ -58,41 +62,50 @@ class Bot:
             but_wthr = types.InlineKeyboardButton(text='Угадать город по погоде', callback_data='wthr')
             but_cd = types.InlineKeyboardButton(text='Угадать страну по описанию из ЕГЭ', callback_data='cd')
             but_rd = types.InlineKeyboardButton(text='Угадать регион России по описанию из ЕГЭ', callback_data='rd')
-            but_flg = types.InlineKeyboardButton(text='Угадать Страну по флагу', callback_data='flg')
-            but_shp = types.InlineKeyboardButton(text='Угадать Страну по очертаниям', callback_data='shp')
-            markup.add(but_cC, but_tc, but_wthr, but_cd, but_rd, but_flg, but_shp)
+            but_flg = types.InlineKeyboardButton(text='Угадать страну по флагу', callback_data='flg')
+            but_shp = types.InlineKeyboardButton(text='Угадать страну по очертаниям', callback_data='shp')
+            but_vars = types.InlineKeyboardButton(text='Добавить/убрать варианты ответа', callback_data='change_vars')
+            markup.add(but_cC, but_tc, but_wthr, but_cd, but_rd, but_flg, but_shp, but_vars)
             self.bot.send_message(chat_id, message_text, reply_markup=markup)
-
-
 
     def reply_inline_call(self, call):
         chat_id = call.message.chat.id
+        if chat_id not in self.chats: self.chats[chat_id] = Chat()
         if call.data == 'help':
             self.bot.edit_message_reply_markup(call.message.chat.id, call.message.id, reply_markup=None)
             self.print_special_message(chat_id, 'help')
         elif call.data == 'yes':
             self.bot.edit_message_reply_markup(call.message.chat.id, call.message.id, reply_markup=None)
-            if chat_id not in self.chats:
-                self.chats[chat_id] = Chat()
             self.chats[chat_id].premessage = 'Отлично!'
             self.print_special_message(chat_id, 'choose_category')
+        elif call.data == 'change_vars':
+            self.chats[chat_id].ans_hidden = not self.chats[chat_id].ans_hidden
+            self.bot.edit_message_reply_markup(call.message.chat.id, call.message.id, reply_markup=None)
+            if self.chats[chat_id].ans_hidden:
+                self.chats[chat_id].premessage = 'Варианты ответа выключены'
+            else:
+                self.chats[chat_id].premessage = 'Варианты ответа включены'
+            self.print_special_message(chat_id, 'choose_category')
+        elif call.data == 'correct_ans':
+            self.chats[chat_id].premessage = 'Верно!'
+            self.ask(call.message.chat.id, self.chats[chat_id].category, self.chats[chat_id].ans_hidden)
+        elif call.data == 'wrong_ans':
+            self.chats[chat_id].premessage = 'Неверно! Правильный ответ: ' + self.chats[chat_id].waiting_answer
+            self.ask(call.message.chat.id, self.chats[chat_id].category, self.chats[chat_id].ans_hidden)
         elif call.data in self.question_types:
             self.bot.edit_message_reply_markup(call.message.chat.id, call.message.id, reply_markup=None)
-            if chat_id not in self.chats:
-                self.chats[chat_id] = Chat()
+            if chat_id not in self.chats: self.chats[chat_id] = Chat()
             self.chats[chat_id].category = call.data
-            self.ask(chat_id, call.data)
+            self.ask(chat_id, call.data, self.chats[chat_id].ans_hidden)
         elif call.data == 'change_category':
-            if chat_id not in self.chats:
-                self.chats[chat_id] = Chat()
+            if chat_id not in self.chats: self.chats[chat_id] = Chat()
             self.chats[call.message.chat.id].premessage = ''
             self.chats[call.message.chat.id].waiting_answer = None
             self.chats[call.message.chat.id].category = None
             self.bot.edit_message_reply_markup(call.message.chat.id, call.message.id, reply_markup=None)
             self.print_special_message(chat_id, 'choose_category')
         elif call.data == 'exit':
-            if chat_id not in self.chats:
-                self.chats[chat_id] = Chat()
+            if chat_id not in self.chats: self.chats[chat_id] = Chat()
             self.chats[call.message.chat.id].premessage = ''
             self.chats[call.message.chat.id].waiting_answer = None
             self.chats[call.message.chat.id].category = None
@@ -100,46 +113,50 @@ class Bot:
             self.bot.edit_message_reply_markup(call.message.chat.id, call.message.id, reply_markup=None)
             pass
 
-    def ask(self, chat_id, category='cC'):
-        question, answer = generate_question(category)
-        if chat_id not in self.chats:
-            self.chats[chat_id] = Chat()
-        question_image = question
+    def ask(self, chat_id, category='cC', ans_hidden=False):
+        if chat_id not in self.chats: self.chats[chat_id] = Chat()
+        question, answer, vars = generate_question(category, ans_hidden=ans_hidden)
         question = self.chats[chat_id].premessage + '\n\n' + question
-        try:
-            markup = types.InlineKeyboardMarkup()
-            but_change = types.InlineKeyboardButton(text='Хочу сменить категорию вопросов',
-                                                    callback_data='change_category')
-            but_exit = types.InlineKeyboardButton(text='Больше не хочу отвечать на вопросы', callback_data='exit')
-            markup.add(but_change, but_exit)
-            if category in ['flg', 'shp']:
-                print(question)
-                self.bot.send_photo(chat_id, photo=question_image, caption='Угадайте страну:', reply_markup=markup)
-            else:
-                self.bot.send_message(chat_id, question, reply_markup=markup)
-            self.chats[chat_id].waiting_answer = answer
-        except Exception as e:
-            print("{!s}\n{!s}".format(type(e), str(e)))
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        but_change = types.InlineKeyboardButton(text='Хочу сменить категорию вопросов',
+                                                callback_data='change_category')
+        but_exit = types.InlineKeyboardButton(text='Больше не хочу отвечать на вопросы', callback_data='exit')
+        if not ans_hidden:
+            but_correct = types.InlineKeyboardButton(text=answer, callback_data='correct_ans')
+            but_vars = [but_correct]
+            for var in vars:
+                new_but = types.InlineKeyboardButton(text=var, callback_data='wrong_ans')
+                but_vars.append(new_but)
+            random.shuffle(but_vars)
+            markup.add(*but_vars)
+        self.chats[chat_id].waiting_answer = answer
+        markup.add(but_change)
+        markup.add(but_exit)
+        if category in ['flg', 'shp']:
+            self.bot.send_photo(chat_id, photo=question, caption='Угадайте страну:', reply_markup=markup)
+        else:
+            self.bot.send_message(chat_id, question, reply_markup=markup)
+
 
     def check_answer(self, message):
         chat_id = message.chat.id
         received_answer = good_name(message.text.strip())
-        if chat_id not in self.chats:
-            self.chats[chat_id] = Chat()
+        if chat_id not in self.chats: self.chats[chat_id] = Chat()
         if self.chats[chat_id].waiting_answer is None:
             self.print_special_message(message.chat.id, 'unexpected')
         elif (isinstance(self.chats[chat_id].waiting_answer, str)
               and received_answer == good_name(self.chats[chat_id].waiting_answer)) or \
-                (received_answer in good_name(self.chats[chat_id].waiting_answer)):
+                (received_answer in good_name(self.chats[chat_id].waiting_answer).split('|')):
             self.chats[chat_id].premessage = 'Верно!'
             self.chats[chat_id].waiting_answer = None
             self.chats[chat_id].streak += 1
-            self.ask(message.chat.id, self.chats[chat_id].category)
+            self.ask(message.chat.id, self.chats[chat_id].category, self.chats[chat_id].ans_hidden)
         else:
             self.chats[chat_id].premessage = 'Неверно! Правильный ответ: ' + self.chats[chat_id].waiting_answer
             self.chats[chat_id].waiting_answer = None
             self.chats[chat_id].streak = 0
-            self.ask(message.chat.id, self.chats[chat_id].category)
+            self.ask(message.chat.id, self.chats[chat_id].category, self.chats[chat_id].ans_hidden)
+
 
     def start(self):
         self.bot.polling(none_stop=True, interval=0)
--- question_generation.py
+++ question_generation.py
@@ -6,8 +6,7 @@ from datetime import datetime
 
 from database_parser import *
 
-c, cp = get_cp()
-t, tp, tc, cC = get_tpcC()
+t, tp, tc, cC, c = get_tpcC()
 
 
 token_accu = 'gBj1vV4C8jprBzXRFLHpyAriTn7nvO3G'
@@ -44,49 +43,51 @@ def weather_in_city(city):
     return you_weather
 
 
-def generate_question(category):
+def gen_wrong_answers(cat, true_ans):
+    wrong_answers = set()
+    count = 0
+    while count != 3:
+        new_ans = None
+        if cat == 'c':
+            new_ans = random.choice(c)
+        if cat == 'C':
+            new_ans = random.choice(list(cC.values()))
+        elif cat == 't':
+            new_ans = random.choice(t)
+        if new_ans != true_ans and new_ans not in wrong_answers:
+            wrong_answers.add(new_ans)
+            count += 1
+    return list(wrong_answers)
+
+def generate_question(category, ans_hidden=True):
     if category == 'cC': # country -> Capital
         cid = random.randint(0, len(cC.keys()) - 1)
         c_ = c[cid]
         C_ = cC[c_]
-        return 'Назовите столицу ' + c_ + '.', C_
+        return 'Назовите столицу ' + c_ + '.', C_, gen_wrong_answers('C', C_)
     elif category == 'wthr': # weather -> town
         tid = random.randint(0, len(t) - 1)
         t_ = t[tid]
         you_weather = weather_in_city(t_)
-        return 'Угадайте город. Температура: ' + str(you_weather['temp']) + ', а на небе: ' + str(you_weather['sky']), t_
+        return 'Угадайте город. Температура: ' + str(you_weather['temp']) + ', а на небе: ' + str(you_weather['sky']), t_, gen_wrong_answers('t', t_)
     elif category == 'tc': # town -> country
         tid = random.randint(0, len(t) - 1)
         t_ = t[tid]
         c_ = tc[t_]
-        return 'В какой стране находится город ' + t_, c_
+        return 'В какой стране находится город ' + t_, c_, gen_wrong_answers('c', c_)
     elif category == "cd": # country <- description
         with open("cd.json") as f:
             questions = json.load(f)
         cid = random.randint(0, len(questions["questions"]) - 1)
-        return questions["questions"][cid]["question"], questions["questions"][cid]["answer"]
+        return questions["questions"][cid]["question"], questions["questions"][cid]["answer"], []
     elif category == "rd": # region <- description
-        # возврощаемый answer это одна строка вида "Свердловская|Свердловская область' думаю надо сделать
-        # проверку того что ответ - пользователя подстрока правильного
         with open("rd.json") as f:
             questions = json.load(f)
         cid = random.randint(0, len(questions["questions"]) - 1)
-        return questions["questions"][cid]["question"], questions["questions"][cid]["answer"]
-    elif category == "flg":
-        with open("images.json") as f:
-            questions = json.load(f)
-        cid = random.randint(0, len(questions["questions"]) - 1)
-        print(questions["questions"][cid]["flag"], questions["questions"][cid]["name"])
-        return questions["questions"][cid]["flag"], questions["questions"][cid]["name"]
-    elif category == "shp":
-        with open("images.json") as f:
-            questions = json.load(f)
-        cid = random.randint(0, len(questions["questions"]) - 1)
-        print(questions["questions"][cid]["shape"], questions["questions"][cid]["name"])
-        return questions["questions"][cid]["shape"], questions["questions"][cid]["name"]
+        return questions["questions"][cid]["question"], questions["questions"][cid]["answer"], []
     elif category == "rnd": #random question
-        cid = random.choice(["cC", "wthr", "tc", "cd", "rd"])
-        return generate_question(cid)
+        cat = random.choice(["cC", "wthr", "tc", "cd", "rd"])
+        return generate_question(cat)
 
 
 
