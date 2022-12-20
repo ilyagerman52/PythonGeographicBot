149ae32c4d3880e0177396d50d63493f40e2fed2
ilyagerman52 <ivgerman@edu.hse.ru>
2022-12-20 19:01:36 +0300

    utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
 from translate import Translator
+import re
 
-translator = Translator(to_lang='Russian')
 
-def translate():
-    pass
+def translate(en_str):
+    translator = Translator(to_lang='Russian')
+    ru_str = translator.translate(en_str)
+    if ru_str[:3] == 'г. ':
+        ru_str = ru_str[3:]
+    if len(re.findall(r"[а-яA-Я-'.’ ]", ru_str)) != len(ru_str):
+        print(en_str, ru_str)
+        return
+    return ru_str
