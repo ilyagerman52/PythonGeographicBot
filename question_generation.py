b0a9de66649385c606cca9255fe96bd8b853247c
ilyagerman52 <ivgerman@edu.hse.ru>
2022-12-21 02:04:00 +0300

    some fixes

--- question_generation.py
+++ question_generation.py
@@ -9,25 +9,21 @@ def gen_wrong_answers(cat, true_ans):
     count = 0
     while count != 3:
         new_ans = None
+        conn = sqlite3.connect("geonames.db")
+        cur = conn.cursor()
         if cat == 'c':
-            conn = sqlite3.connect("geonames.db")
-            cur = conn.cursor()
             cur.execute("""SELECT c_r
                         FROM countries
                         ORDER BY RANDOM()
                         LIMIT 1""")
             new_ans = cur.fetchone()[0]
         if cat == 'C':
-            conn = sqlite3.connect("geonames.db")
-            cur = conn.cursor()
             cur.execute("""SELECT Cap_r
                         FROM countries
                         ORDER BY RANDOM()
                         LIMIT 1""")
             new_ans = cur.fetchone()[0]
         elif cat == 't':
-            conn = sqlite3.connect("geonames.db")
-            cur = conn.cursor()
             cur.execute("""SELECT t_r
                         FROM towns
                         ORDER BY RANDOM()
@@ -40,9 +36,9 @@ def gen_wrong_answers(cat, true_ans):
 
 
 def generate_question(category):
+    conn = sqlite3.connect("geonames.db")
+    cur = conn.cursor()
     if category == 'cC':  # country -> Capital
-        conn = sqlite3.connect("geonames.db")
-        cur = conn.cursor()
         cur.execute("""SELECT c_r, Cap_r
                         FROM countries
                         ORDER BY RANDOM()
@@ -50,8 +46,6 @@ def generate_question(category):
         c_r, C_r = cur.fetchone()
         return 'Назовите столицу ' + c_r + '.', C_r, gen_wrong_answers('C', C_r)
     elif category == 'wthr':  # weather -> town
-        conn = sqlite3.connect("geonames.db")
-        cur = conn.cursor()
         cur.execute("""SELECT t_r
                         FROM towns
                         ORDER BY RANDOM()
@@ -61,8 +55,6 @@ def generate_question(category):
         return 'Угадайте город. Температура: ' + str(you_weather['temp']) + ', а на небе: ' + str(
             you_weather['sky']), t_, gen_wrong_answers('t', t_)
     elif category == 'tc':  # town -> country
-        conn = sqlite3.connect("geonames.db")
-        cur = conn.cursor()
         cur.execute("""SELECT t_r, c_r
                         FROM towns
                         ORDER BY RANDOM()
@@ -70,16 +62,12 @@ def generate_question(category):
         temp = cur.fetchone()
         return 'В какой стране находится город ' + temp[0], temp[1], gen_wrong_answers('c', temp[1])
     elif category == "cd":  # country <- description
-        conn = sqlite3.connect("geonames.db")
-        cur = conn.cursor()
         cur.execute("""SELECT *
                         FROM ege_17
                         ORDER BY RANDOM()
                         LIMIT 1""")
         return *cur.fetchone(), []
     elif category == "rd":  # region <- description
-        conn = sqlite3.connect("geonames.db")
-        cur = conn.cursor()
         cur.execute("""SELECT *
                         FROM ege_18
                         ORDER BY RANDOM()
@@ -89,8 +77,6 @@ def generate_question(category):
         cat = random.choice(["cC", "wthr", "tc", "cd", "rd", "flg", "brd"])
         return generate_question(cat)
     elif category == "flg":
-        conn = sqlite3.connect("geonames.db")
-        cur = conn.cursor()
         cur.execute("""SELECT c_r, flg
                         FROM countries
                         ORDER BY RANDOM()
@@ -98,8 +84,6 @@ def generate_question(category):
         c_r, flg = cur.fetchone()
         return flg, c_r, gen_wrong_answers('c', c_r)
     elif category == "brd":
-        conn = sqlite3.connect("geonames.db")
-        cur = conn.cursor()
         cur.execute("""SELECT c_r, brd
                         FROM countries
                         ORDER BY RANDOM()
